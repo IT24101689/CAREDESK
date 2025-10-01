0fac076c14cd1e4d8ac660a6564e9ffd7dccf220
Hansaja Suriyakumara <it24102014@my.sliit.lk>
2025-10-01 20:49:53 +0530

    Agent imports

--- src/main/java/com/caredesk/caredesk/controller/AgentController.java
+++ src/main/java/com/caredesk/caredesk/controller/AgentController.java
@@ -1,4 +1,15 @@
 package com.caredesk.caredesk.controller;
 
+import com.caredesk.caredesk.model.Ticket;
+import com.caredesk.caredesk.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/agent")
 public class AgentController {
 }
