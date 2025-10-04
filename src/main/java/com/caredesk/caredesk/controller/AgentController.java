1ba3c509d452c75ec84b48fce259d4e9bb389255
Hansaja Suriyakumara <it24102014@my.sliit.lk>
2025-10-04 10:25:29 +0530

    imp

--- src/main/java/com/caredesk/caredesk/controller/AgentController.java
+++ src/main/java/com/caredesk/caredesk/controller/AgentController.java
@@ -5,6 +5,7 @@ import com.caredesk.caredesk.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 import java.util.List;
 import java.util.Optional;
